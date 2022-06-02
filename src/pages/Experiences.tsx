928b63f29f862417cc4af00ff31446703615caa0
vicky indiar <vickynewonline@gmail.com>
2022-06-02 12:36:24 +0700

    update

--- src/pages/Experiences.tsx
+++ src/pages/Experiences.tsx
@@ -12,7 +12,7 @@ const Experiences:FC = () => (
       <header>
         <div className="title">
           <h2 data-testid="heading"><Link to="/experiences">Experiences</Link></h2>
-          <p>A selection of projects that I&apos;m not too ashamed of</p>
+          <p>A selections of projects that I have worked on and or i am in</p>
         </div>
       </header>
       {data.map((exp) => (
